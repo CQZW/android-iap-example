9f9f5fb30871bd4f682a1fac68e690642d93ded7
hyochan <dooboolab@gmail.com>
2017-10-22 17:46:28 +0900

    Added test uis to prepare implementation.

--- app/src/main/java/com/dooboolab/iapexample/MainActivity.java
+++ app/src/main/java/com/dooboolab/iapexample/MainActivity.java
@@ -5,20 +5,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-  Button btn;
+  private Button btn1;
+  private Button btn2;
+  private Button btn3;
+  private Button btn4;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    btn = (Button) findViewById(R.id.btn);
-    btn.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
+    btn1 = (Button) findViewById(R.id.btn1);
+    btn2 = (Button) findViewById(R.id.btn2);
+    btn3 = (Button) findViewById(R.id.btn3);
+    btn4 = (Button) findViewById(R.id.btn4);
 
-      }
-    });
+    btn1.setOnClickListener(this);
+    btn2.setOnClickListener(this);
+    btn3.setOnClickListener(this);
+    btn4.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.btn1:
+        break;
+      case R.id.btn2:
+        break;
+      case R.id.btn3:
+        break;
+      case R.id.btn4:
+        break;
+    }
   }
 }
