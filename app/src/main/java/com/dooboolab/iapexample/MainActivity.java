f0a2512fd86e9655ed0eec0136a847657b0ca827
hyochan <dooboolab@gmail.com>
2017-10-22 10:41:17 +0900

    Initial commit.

---
+++ app/src/main/java/com/dooboolab/iapexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dooboolab.iapexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
